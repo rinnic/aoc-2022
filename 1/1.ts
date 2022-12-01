50ee8e40e971fd918d4e386a8db144d342b696e9
Nicola Rinaldi <rinaldi.nicola@outlook.it>
2022-12-01 19:38:47 +0100

    day1 solution

---
+++ 1/1.ts
@@ -0,0 +1,18 @@
+import {readFileSync} from 'fs'
+
+const content = readFileSync('1.in', 'utf-8')
+
+const sumElfCalories = (elfLines: string) => {
+  return elfLines.split("\n")
+                 .map(el => parseInt(el))
+                 .reduce((x, y) => x + y)
+}
+
+const elvesCalories = content.split("\n\n")
+                             .map(el => sumElfCalories(el))
+                             .sort((x,y) => y - x)  //not useful for part 1
+
+console.log("First part:", Math.max(...elvesCalories))           
+console.log("Second part:", elvesCalories.slice(0, 3).reduce((x,y) => x + y))
+
+
