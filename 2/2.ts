9275570d05b5b5057a94c50fee7658298529ec22
Nicola Rinaldi <rinaldi.nicola@outlook.it>
2022-12-02 15:01:14 +0100

    promoted last solution to main one

--- 2/2.ts
+++ 2/2.ts
@@ -1,85 +1,40 @@
-import { defaultMaxListeners } from "events";
 import { readFileSync } from "fs";
 
 const input = readFileSync("2.in", "utf-8");
 
-const paired = input.split("\n").map((row) => row.split(" "));
+const choiceDict1 = new Map<string, number>([
+  ["X", 1],
+  ["Y", 2],
+  ["Z", 3]
+])
 
-const choice = (row: string[]): number => {
-  switch (row[1]) {
-    case "X":
-      return 1;
-    case "Y":
-      return 2;
-    case "Z":
-      return 3;
-    default:
-      return -1;
-  }
-};
+const pointDict1 = new Map<string, number>([
+  ["A X", 3], ["A Y", 6], ["A Z", 0],
+  ["B X", 0], ["B Y", 3], ["B Z", 6],
+  ["C X", 6], ["C Y", 0], ["C Z", 3]
+])
 
-const part1points = (row: string[]): number => {
-  const n = row[0];
-  const m = row[1];
-  switch (n) {
-    case "A":
-      if (m === "X") return 3;
-      else if (m === "Y") return 6;
-      else return 0;
-    case "B":
-      if (m === "X") return 0;
-      else if (m === "Y") return 3;
-      else return 6;
-    case "C":
-      if (m === "X") return 6;
-      else if (m === "Y") return 0;
-      else return 3;
-  }
-  return -1
-};
+const pointDict2 = new Map<string, number>([
+  ["X", 0],
+  ["Y", 3],
+  ["Z", 6]
+])
 
-const part2points = (row: string[]): number => {
-  const m = row[1];
-  switch (m) {
-    case "X": return 0
-    case "Y": return 3
-    case "Z": return 6
-  }
-  return -1
-};
+const choiceDict2 = new Map<string, number>([
+  ["A X", 3], ["A Y", 1], ["A Z", 2],
+  ["B X", 1], ["B Y", 2], ["B Z", 3],
+  ["C X", 2], ["C Y", 3], ["C Z", 1]
+])
 
-const makeChoice = (row: string[]): string[] => {
-  const n = row[0];
-  const m = row[1];
-  let newM = "";
-  switch (n) {
-    case "A":
-      if (m === "Z") newM = "Y";
-      else if (m === "Y") newM = "X";
-      else newM = "Z";
-      break;
-    case "B":
-      if (m === "Z") newM = "Z";
-      else if (m === "Y") newM = "Y";
-      else newM = "X";
-      break;
-    case "C":
-      if (m === "Z") newM = "X";
-      else if (m === "Y") newM = "Z";
-      else newM = "Y";
-  }
-  return [n, newM]
-};
+let part1 = 0
+input.split("\n").forEach((el) => {
+  part1 +=  (pointDict1.get(el) || 0) + (choiceDict1.get(el.split(" ")[1]) || 0)
+})
 
-const part1 = paired.map(row => choice(row)).reduce((a,b) => a + b) +
-              paired.map(row => part1points(row)).reduce((a,b) => a + b)
+let part2 = 0
+input.split("\n").forEach((el) => {
+  part2 +=  (pointDict2.get(el.split(" ")[1]) || 0) + (choiceDict2.get(el) || 0)
+})
 
-
-const part2 = paired.map(row => part2points(row)).reduce((a,b) => a + b) + 
-              paired.map(row => makeChoice(row)).map(row => choice(row)).reduce((a,b) => a + b)
-
-
-console.log("part1: ", part1)
-console.log("part2: ", part2)
-
-
+console.log("part1:", part1)
+console.log("part2:", part2)
