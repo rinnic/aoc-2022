39ea49f9552525f091fd4600135279eb90453425
Nicola Rinaldi <rinaldi.nicola@outlook.it>
2023-01-04 22:51:14 +0100

    15 part 1

--- 15/15.ts
+++ 15/15.ts
@@ -2,20 +2,52 @@ import { readFileSync } from "fs";
 
 const input = readFileSync("15.in", "utf-8").split("\n");
 
+const manhattan = (p1: number[], p2: number[]): number => {
+  return Math.abs(p1[0] - p2[0]) + Math.abs(p1[1] - p2[1]);
+};
+
 const distances: number[][][] = [];
 
 input.forEach((row) => {
   const words = row.split(" ");
+
   distances.push([
     [
-      parseInt(words[2].match(/\d+/g)?.at(0) || "0"),
-      parseInt(words[3].match(/\d+/g)?.at(0) || "0"),
+      parseInt(words[2].split("=")[1]),
+      parseInt(words[3].split("=")[1]),
     ],
     [
-      parseInt(words[8].match(/\d+/g)?.at(0) || "0"),
-      parseInt(words[9].match(/\d+/g)?.at(0) || "0"),
+      parseInt(words[8].split("=")[1]),
+      parseInt(words[9].split("=")[1]),
     ],
   ]);
 });
 
-console.log(distances);
+const Y = 2000000
+const beacons: string[] = distances.map(pair => `${pair[1][0]};${pair[1][1]}`)
+const map = new Set<string>();
+
+distances.forEach(([[x1, y1], [x2, y2]]) => {
+  const d = manhattan([x1, y1], [x2, y2]);
+  for (let i = -d; i <= 0; i++) {
+    if(y1+i != Y) continue
+    const width = i + d;
+    for (let j = -width; j <= width; j++) {
+      if(beacons.includes(`${x1+j};${y1 + i}`) == false){
+        map.add(`${x1+j};${y1 + i}`)
+      }
+    }
+  }
+  for (let i = d; i > 0; i--) {
+    if(y1+i != Y) continue
+    const width = d - i;
+    for (let j = -width; j <= width; j++) {
+      if(beacons.includes(`${x1+j};${y1 + i}`) == false){
+        map.add(`${x1+j};${y1 + i}`)
+      }
+    }
+  }
+});
+
+const part1 = map.size
+console.log(part1)
